4fa773906a89fd972509694c58f1e307c7cc7cb5
lee-shun <lee970802@163.com>
2022-03-28 11:15:07 -0400

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,63 +1,79 @@
 -- install packer
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 -- plugins
 require('packer').startup( function(use)
-    use { 'wbthomason/packer.nvim'}
+  use { 'wbthomason/packer.nvim'}
 
-    -- commom libs
-    use {'nvim-lua/popup.nvim'}
-    use {'nvim-lua/plenary.nvim'}
+  -- commom libs
+  use {'nvim-lua/popup.nvim'}
+  use {'nvim-lua/plenary.nvim'}
 
-    -- UI
-    use {'arcticicestudio/nord-vim'}
+  -- UI
+  use {'arcticicestudio/nord-vim'}
 
-    use { 'glepnir/galaxyline.nvim',
+  use {'glepnir/galaxyline.nvim',
     branch = 'main',
     requires = {'kyazdani42/nvim-web-devicons', opt = true},
     config = function() require('plugins.configs.statusline.eviline') end}
 
 
-    use { 'glepnir/dashboard-nvim',
+  use {'glepnir/dashboard-nvim',
     config = function() require('plugins.configs.dashboard') end}
 
-    -- file navgative
-    use { 'kyazdani42/nvim-tree.lua',
+  -- file navgative
+  use {'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     config = function() require('plugins.configs.nvimtree') end }
 
-    use {'nvim-telescope/telescope.nvim',
+  use {'nvim-telescope/telescope.nvim',
     config = function() require('plugins.configs.telescope') end }
 
-    -- general enchance
-    use {'windwp/nvim-autopairs',
+  -- general enchance
+  use {'windwp/nvim-autopairs',
     config = function() require('nvim-autopairs').setup{} end }
 
-    use {'numToStr/Comment.nvim',
+  use {'numToStr/Comment.nvim',
     config = function() require('Comment').setup{} end }
 
-    use {'norcalli/nvim-colorizer.lua',
+  use {'norcalli/nvim-colorizer.lua',
     config = function() require('colorizer').setup{} end }
 
+  use {'nvim-treesitter/nvim-treesitter', run =':TSUpdate',
+    config = function() require('plugins.configs.treesitter') end }
 
-    -- lsp
-    use {'neovim/nvim-lspconfig',
+  use {'nvim-treesitter/playground'}
+
+  use {'p00f/nvim-ts-rainbow'}
+  use {'voldikss/vim-floaterm'}
+
+
+  -- languages
+  use {'sbdchd/neoformat'}
+
+  -- lsp
+  use {'neovim/nvim-lspconfig',
     config = function() require('plugins.configs.lsp.lspconfig') end }
-    use { 'hrsh7th/cmp-nvim-lsp'}
-    use { 'hrsh7th/cmp-buffer'  }
-    use { 'hrsh7th/cmp-path'    }
-    use { 'hrsh7th/cmp-cmdline' }
-    use { 'hrsh7th/nvim-cmp',
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+  use {'hrsh7th/nvim-cmp',
     config = function() require('plugins.configs.lsp.cmp') end }
-    use {'hrsh7th/cmp-vsnip'}
-    use {'hrsh7th/vim-vsnip'}
+  use {'hrsh7th/cmp-vsnip'}
+  use {'hrsh7th/vim-vsnip'}
+  -- TODO: there is a bug ...
+  use {'Thiago4532/lsp-semantic.nvim',}
+  use {'ahmedkhalf/project.nvim',
+    config = function() require('plugins.configs.lsp.project') end }
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 require('plugins.configs.color')
