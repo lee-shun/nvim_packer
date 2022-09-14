8331313a03cc0dd99f518663937437233db36555
lee-shun <lee970802@163.com>
2022-09-14 10:35:41 -0400

    u

--- lua/plugins/configs/alpha.lua
+++ lua/plugins/configs/alpha.lua
@@ -29,10 +29,22 @@ dashboard.section.buttons.val = {
 	dashboard.button("q", "  Quit Nvim", "<cmd>qa<CR>"),
 }
 
-local handle = io.popen("fortune")
-local fortune = handle:read("*a")
-handle:close()
-dashboard.section.footer.val = fortune
+local function footer()
+	local total_plugins = #vim.tbl_keys(packer_plugins)
+	return "   Have Fun with neovim"
+		.. "   v"
+		.. vim.version().major
+		.. "."
+		.. vim.version().minor
+		.. "."
+		.. vim.version().patch
+		.. "   "
+		.. total_plugins
+		.. " plugins"
+end
+
+dashboard.section.footer.val = footer()
+dashboard.section.footer.opts.hl = "Function"
+
 dashboard.config.opts.noautocmd = true
-vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.config)
