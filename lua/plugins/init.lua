a455d7cebb558d70a6b036deb822b695a8a135b7
lee-shun <lee970802@163.com>
2022-10-16 19:29:59 -0400

    u

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -404,7 +404,20 @@ require("packer").startup(function(use)
 
 	use({ "thibthib18/ros-nvim", ft = { "rosmsg", "cpp" } })
 
-	use({ "lervag/vimtex", ft = "tex" })
+	use({
+		"lervag/vimtex",
+		ft = "tex",
+		config = function()
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_quickfix_mode = 0
+			vim.g.vimtex_mappings_enabled = 0
+			vim.g.vimtex_imaps_enabled = 0
+			vim.g.vimtex_text_obj_enabled = 0
+			vim.g.vimtex_fold_enabled = 1
+			vim.g.tex_conceal = "abdmg"
+			vim.g.vimtex_format_enabled = 1
+		end,
+	})
 
 	use({
 		"iamcco/markdown-preview.nvim",
