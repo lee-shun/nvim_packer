027fbf620b03e883626b3ea6de561b4abf015ad1
ShawnLee <lee970802@163.com>
2023-02-12 10:15:00 -0500

    update

--- lua/plugins/configs/lsp/null_ls.lua
+++ lua/plugins/configs/lsp/null_ls.lua
@@ -1,9 +1,8 @@
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
-		null_ls.builtins.diagnostics.cppcheck,
-		-- null_ls.builtins.diagnostics.cspell,
-		null_ls.builtins.diagnostics.markdownlint,
+		null_ls.builtins.diagnostics.cpplint,
+		null_ls.builtins.diagnostics.markdownlint_cli2,
 		null_ls.builtins.diagnostics.pylint,
 	},
 })
